b230e4211e75a56e5a68e09c4882441ea5513841
Mark A Guza <guzamaguza@gmail.com>
2020-05-22 17:12:28 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,13 +12,15 @@ class ApplicationController < Sinatra::Base
   end
 =end
 
-  get '/recipes/new' do
-    erb :new
-  end
+
 
   get '/recipes' do
     @recipes = Recipe.all
     erb :index
+  end
+
+  get '/recipes/new' do
+    erb :new
   end
 
   post '/recipes' do
